632800566cccaffa73737a66c75af7d3775f9873
unknown <bmac2700@proton.me>
2022-09-07 17:54:04 +0300

    Fixed scout bunnyhop issue

--- src/modules/movement/bunnyhop.rs
+++ src/modules/movement/bunnyhop.rs
@@ -1,19 +1,39 @@
+use std::sync::Mutex;
+
 use crate::{
     cheat::GlobalData,
     classes::{cbaseplayer::CBasePlayer, cusercmd::CUserCMD},
 };
 
+const IN_JUMP: i32 = 1 << 1;
+
+const FL_ONGROUND: i32 = 1 << 0;
+
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref LAST_JUMPED: Mutex<bool> = Mutex::new(true);
+}
+
 pub fn run(global_data: &GlobalData, cmd: &mut CUserCMD) {
+    let mut last_jumped = LAST_JUMPED.lock().unwrap();
+
     let local_player = CBasePlayer::new(
         global_data
             .client_entity_list_interface
             .get_client_entity(1),
     );
 
-    if (local_player.get_flags() & (1 << 0)) == 0 {
+    if (cmd.get_buttons() & IN_JUMP) == IN_JUMP {
         let mut buttons = cmd.get_buttons();
-        buttons &= !(1 << 1);
+
+        if !*last_jumped && (local_player.get_flags() & FL_ONGROUND) != FL_ONGROUND {
+            buttons &= !IN_JUMP;
+        } else if *last_jumped {
+            *last_jumped = false;
+        }
 
         cmd.set_buttons(buttons);
+    } else if !*last_jumped {
+        *last_jumped = true;
     }
 }
