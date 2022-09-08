41109883b8b08501c30f7ddf87c026c6a7b33456
unknown <bmac2700@proton.me>
2022-09-08 21:40:06 +0300

    Little bit better antibackstab

--- src/hooks/hooked_functions/create_move.rs
+++ src/hooks/hooked_functions/create_move.rs
@@ -1,4 +1,5 @@
 use crate::{classes::cusercmd::CUserCMD, math::vector3::recalculate_viewangle};
+use std::arch::asm;
 
 pub extern "thiscall" fn create_move_hook(
     caller_class: *const u8,
@@ -17,6 +18,26 @@ pub extern "thiscall" fn create_move_hook(
     if user_cmd as usize == 0 || cmd.get_command_number() == 0 {
         return original_return_value;
     }
+
+    let mut frame_pointer: usize = 0;
+    let mut send_packet: *mut bool = 0 as _;
+
+    unsafe {
+        asm!(
+            "mov {frame_pointer}, ebp",
+            frame_pointer = out(reg) frame_pointer,
+        );
+
+        send_packet = frame_pointer as *mut bool;
+
+        for _ in 0..3 {
+            send_packet = *(send_packet as *const usize) as *mut bool;
+        }
+
+        send_packet = (send_packet as usize - 0x1) as *mut bool;
+    }
+
+    let mut safe_send_packet: bool = unsafe { *send_packet };
 
     let old_viewangle = cmd.get_view_angle();
     let old_forward_move = cmd.get_forward_move();
@@ -37,6 +58,8 @@ pub extern "thiscall" fn create_move_hook(
 
     cmd.set_forward_move(forward_move);
     cmd.set_side_move(side_move);
+
+    unsafe { *send_packet = safe_send_packet };
 
     return false;
 }
--- src/modules/misc/anti_backstab.rs
+++ src/modules/misc/anti_backstab.rs
@@ -3,45 +3,80 @@ use crate::{
     classes::{cbaseentity::CBaseEntity, cbaseplayer::CBasePlayer, cusercmd::CUserCMD},
 };
 
-pub fn run(global_data: &GlobalData, cmd: &mut CUserCMD) {
-    // Gets the local player, so we can read its values.
-    let local_player = CBasePlayer::new(
-        global_data
-            .client_entity_list_interface
-            .get_client_entity(1),
-    );
+const IN_ATTACK: i32 = 1 << 0;
 
+pub fn run(global_data: &GlobalData, cmd: &mut CUserCMD) {
+
+    if (cmd.get_buttons() & IN_ATTACK) == IN_ATTACK {
+        return;
+    }
+
+    // Gets the local entity, so we can read its values.
     let local_entity = CBaseEntity::new(
         global_data
             .client_entity_list_interface
             .get_client_entity(1),
     );
 
-    for i in 2..32 {
-        let entity_ptr = global_data
-            .client_entity_list_interface
-            .get_client_entity(i);
+    let mut closest_entity_id = 0;
+    let mut closest_entity_distance = f32::MAX;
 
-        if entity_ptr.is_null() {
+    // Loop through every player
+    for i in 2..32 {
+
+        // Get the entity handle of the player
+        let entity = CBaseEntity::new(
+            global_data
+                .client_entity_list_interface
+                .get_client_entity(i),
+        );
+
+        // Check if the entity handle is null
+        if entity.object_address.is_null() {
             continue;
         }
 
-        let entity = CBaseEntity::new(entity_ptr);
-        let player = CBasePlayer::new(entity_ptr);
-
+        // Check if the player is in our team, if it is then we will skip it
         if local_entity.get_team_num() == entity.get_team_num() {
             continue;
         }
 
+        // Calculate the distance between us and the other player
         let distance = local_entity
             .get_origin()
             .calculate_distance(&entity.get_origin());
 
-        if distance < 250.0 {
-            let view_angle = entity
-                .get_origin()
-                .calculate_angle(&local_entity.get_origin());
-            cmd.set_view_angle(view_angle);
+        // If this player is closer than any player before, then we will set it as the closest one
+        if distance < closest_entity_distance {
+            closest_entity_id = i;
+            closest_entity_distance = distance;
         }
+    }
+
+    // Get the closest entity player as a object
+    let entity = CBaseEntity::new(
+        global_data
+            .client_entity_list_interface
+            .get_client_entity(closest_entity_id),
+    );
+
+    // Check if the entity handle is null
+    if entity.object_address.is_null() {
+        return;
+    }
+
+    // If the closest player is closer than 200 (units ?)
+    if closest_entity_distance < 200.0 {
+
+        // Calculate the a view angle where the we look at the closest player
+        let mut view_angle = local_entity
+            .get_origin()
+            .calculate_angle(&entity.get_origin());
+
+        // Setting the x value of the view angle to -90 makes backstabbing pretty much impossible, but also scrables your own movement for some reason
+        view_angle.x = -90.0;
+
+        // Set the view angle to our new one
+        cmd.set_view_angle(view_angle);
     }
 }
